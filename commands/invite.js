43399e14a8b0dc5764c2e41182c2d371f895dcc7
TH3an <71697130+TH3an@users.noreply.github.com>
2021-01-19 20:09:16 +0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,7 +6,7 @@ module.exports.run = (client, message, args) => {
       let embed = new Discord.RichEmbed()
         .setColor('RANDOM')
         .setAuthor('Support our bot!', client.user.displayAvatarURL)
-        .addField('FUN & MOD BOT', '[INVITE HERE](https://discord.com/oauth2/authorize?client_id=758222639214624788&scope=bot&permissions=2146958847)')
+        .addField('FUN & MOD BOT', '[INVITE HERE](https://discord.com/api/oauth2/authorize?client_id=797721071788490802&permissions=1875378119&scope=bot)')
         .setFooter(`© Cryptonix X Mod Bot by ${customisation.ownername}`);
       
       message.channel.send(embed);
