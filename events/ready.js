00d3bf3c926a9596aac094b98a49b638bf30ec47
TH3an <71697130+TH3an@users.noreply.github.com>
2021-01-20 20:21:54 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -8,7 +8,7 @@ const activities = require("../database/status.json");
 client.setInterval(() => {
     const activity = activities[Math.floor(Math.random() * activities.length)];
     client.user.setActivity(`${config.prefix}help | ${activity.text}`, {
-      status: 'ldle',
+      status: 'online',
       name: 'Bot bek sloy zin',
       type: activity.type,
     });
