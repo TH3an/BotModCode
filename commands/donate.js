5ce162078d097f79c1cb973bf2e9f8f7b7c44a67
TH3an <71697130+TH3an@users.noreply.github.com>
2021-01-20 19:54:05 +0700

    Update donate.js

--- commands/donate.js
+++ commands/donate.js
@@ -4,7 +4,7 @@ const customisation = require('../customisation.json');
 exports.run = async(client, msg, args) => {
   let embed = new Discord.RichEmbed()
   .setColor('RANDOM')
-  .setAuthor("DONATE SAITAMA BOT")
+  .setAuthor("DONATE THUNTHEAN BOT", client.user.displayAvatarUrl)
   .addField('ABA', '❌')
   .addField('PAYPAL', '❌')
   .setFooter(`© Cryptonix X Mod Bot by ${customisation.ownername}`);
