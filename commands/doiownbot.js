69052c2788cd51b139b5c936668f4c9d76200856
TH3an <71697130+TH3an@users.noreply.github.com>
2021-01-20 19:34:39 +0700

    Update doiownbot.js

--- commands/doiownbot.js
+++ commands/doiownbot.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
+const config = require('../config.json');
 
 
 exports.run = (client, message, args) => {
-     if (message.author.id !== '627717611853185026') return message.channel.sendMessage("No, lol ❎");
+     if (message.author.id !== `${config.owner_id}`) return message.channel.sendMessage("No, lol ❎");
      message.channel.send('Yes **Vuth** you Own the bot ✅');
 }
