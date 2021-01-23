2ea1eae100d1cfd8b4a09c93c709dba217d1c0ca
TH3an <71697130+TH3an@users.noreply.github.com>
2021-01-24 01:27:09 +0700

    Update shutdown.js

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-    if(message.author.id !== "627717611853185026") return message.channel.send(`**»** ${message.author}, you don't have permission to do that❌`);
+    if(message.author.id !== "597271012979113984") return message.channel.send(`**»** ${message.author}, you don't have permission to do that❌`);
                console.clear();
                client.destroy()
        return;
