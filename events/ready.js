878d715cb4ff75a7d17bee8a87599bfc838f7433
TH3an <71697130+TH3an@users.noreply.github.com>
2021-01-24 14:38:21 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ module.exports = (client) => {
   console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
 
 const activities = require("../database/status.json");
-  const random = [`On ${client.guilds.size} Servers`, `${client.channels.size} Channels!`, `${client.users.size} users!`, `Made in Cambodia 🇰🇭`];
+  const random = [`On ${client.guilds.size} Servers`, `${client.channels.size} Channels!`, `${client.users.size} users!`, `'Made in Cambodia 🇰🇭'`];
  client.setInterval(() => {
   const beksloy = random[Math.floor(Math.random() * activities.length)];
     const activity = activities[Math.floor(Math.random() * activities.length)];
