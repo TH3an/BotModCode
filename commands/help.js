33e829376b215e291da6f67f90ed43d877902c43
TH3an <71697130+TH3an@users.noreply.github.com>
2021-01-26 11:57:45 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ exports.run = async(client, msg, args) => {
   let embed = new Discord.RichEmbed()
   .setColor('RANDOM')
   .setAuthor('HELP COMMANDS - TheanBotSpport', client.user.displayAvatarUrl)
-  .setDescription('If commands not working usage !bug [report]')
+  .setDescription('If commands not working usage *bug [report]')
   .addField(':smiley: Fun', '`hug`, `kiss`, `kill`, `slap`, `panda`, `rip`, `meter`, `saitama`, `hack`, `meme`, `rabbit`, `anime`, `cat`, `dog`, `marry`, `smoke`, `vote`, `votekick`, `joke`, `icon`, `what`, `bad`, `stonks`, `balance`')
   .addField(':gear: General/Info', '`timer`, `ping`, `channelinfo`, `topinvite`, `botinfo`, `serverinfo`, `userinfo`, `serverinvite`, `guild`, `rolelist`, `level`, `avatar`')
   .addField(':cop: Moderative', '`kick`, `ban`, `unban`, `mute`, `unmute`, `addrole`, `removerole`, `say`, `leave`')
