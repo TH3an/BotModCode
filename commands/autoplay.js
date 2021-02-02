a7f84c105172ef49bdd2360b1f68049d78c63182
TH3an <71697130+TH3an@users.noreply.github.com>
2021-02-02 20:47:31 +0700

    Create autoplay.js

---
+++ commands/autoplay.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js")
+
+module.exports = {
+  name: "autoplay",
+  aliases: ["ap"],
+  run: async (client, message, args) => {
+  if (!message.content.startsWith(config.prefix)) return;
+    const command = args.shift();
+    if (command == "autoplay") {
+        let mode = distube.toggleAutoplay(message);
+        message.channel.send("Set autoplay mode to `" + (mode ? "On" : "Off") + "`");
+    }}
+}
