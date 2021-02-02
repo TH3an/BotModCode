413ad6bbf1b82548e8178e3d92f81cf82c5dbcaf
TH3an <71697130+TH3an@users.noreply.github.com>
2021-02-02 20:44:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
+const DisTube = require("distube")
 const Enmap = require("enmap");
 const fs = require("fs");
-const welcome = require("discord-welcome");
 const leveling = require('discord-leveling');
 const db = require('quick.db');
 
@@ -31,19 +31,6 @@ fs.readdir("./commands/", (err, files) => {
     client.commands.set(commandName, props);
   });
 });
- 
-welcome(client, {
-    "numericserverid1": {
-        privatemsg : "thank for join saitama server",
-        publicmsg : "welcome to my server",
-        publicchannel : "758232502308438016"
-    },
-    "numericserverid2": {
-        privatemsg : "thank for join cubekhmer server",
-        publicmsg : "Welcome on the server",
-        publicchannel : "770126986088808448"
-    }
-})
 
 client.on("message", async message => {
 
@@ -60,11 +47,27 @@ client.on("message", async message => {
     }
 })
 
-//console chatter
-let y = process.openStdin()
-y.addListener("data", res => {
-    let x = res.toString().trim().split(/ +/g)
-    client.channels.get("761111148300337172").send(x.join(" "));
-});
+const status = (queue) => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+client.distube
+    .on("playSong", (message, queue, song) => message.channel.send(
+        `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
+    ))
+    .on("addSong", (message, queue, song) => message.channel.send(
+        `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+    ))
+    .on("${client.emotes.play} |playList", (message, queue, playlist, song) => message.channel.send(
+        `${client.emotes.play} | Play \`${playlist.title}\` playlist (${playlist.total_items} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
+    ))
+    .on("addList", (message, queue, playlist) => message.channel.send(
+        `${client.emotes.success} | Added \`${playlist.title}\` playlist (${playlist.total_items} songs) to queue\n${status(queue)}`
+    ))
+    // DisTubeOptions.searchSongs = true
+    .on("searchResult", (message, result) => {
+        let i = 0;
+        message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`);
+    })
+    // DisTubeOptions.searchSongs = true
+    .on("searchCancel", (message) => message.channel.send(`${client.emotes.error} | Searching canceled`))
+    .on("error", (message, err) => message.channel.send(`${client.emotes.error} | An error encountered: ${err}`));
 
 client.login(config.token);
