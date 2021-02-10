866d5cbfbcfb6bede87463df366c3b46a473d5e1
TH3an <71697130+TH3an@users.noreply.github.com>
2021-02-10 20:28:50 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,14 +5,19 @@ module.exports = (client) => {
   client.channels.get('763697490796609567').send('*Hello!*')
 
 const activities = require("../database/status.json");
-  const random = [`Watchings ${client.users.size} Users!`, `in ${client.channels.size} Channels!`, `on ${client.guilds.size} Servers!`];
+  const random = [
+                  `Watchings ${client.users.size} Users!`,
+                  `in ${client.channels.size} Channels!`,
+                  `on ${client.guilds.size} Servers!`
+                 ];
+  
  client.setInterval(() => {
   const beksloy = random[Math.floor(Math.random() * activities.length)];
     const activity = activities[Math.floor(Math.random() * activities.length)];
     client.user.setActivity(`${config.prefix}help | ${beksloy}`, {
       type: activity.type,
     });
-  }, 30000);
+  }, 20000);
 
 
 }
