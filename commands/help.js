e3e3e93495a5c8a2ce753264e1940ccad26aa216
TH3an <71697130+TH3an@users.noreply.github.com>
2021-02-13 13:07:53 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ const customisation = require('../customisation.json');
 exports.run = async(client, msg, args) => {
   let embed = new Discord.RichEmbed()
   .setColor('RANDOM')
-  .setAuthor('HELP COMMANDS - SAITAMA', "https://cdn.discordapp.com/attachments/758232552636022794/760514173535780964/illustration-logo-cartoon-Saitama-One-Punch-Man-brand-font-product-25409.jpg")
+  .setAuthor('HELP COMMANDS - TheanBot', "https://cdn.discordapp.com/avatars/797721071788490802/07b5f85c95dc00310fb61bf57db3b55f.png?size=2048")
   .setDescription('If commands not working usage s!bug [report]')
   .addField(':smiley: Fun', '`hug`, `kiss`, `kill`, `slap`, `panda`, `rip`, `meter`, `saitama`, `hack`, `meme`, `rabbit`, `anime`, `cat`, `dog`, `marry`, `smoke`, `vote`, `votekick`, `joke`, `icon`, `what`, `bad`, `stonks`')
   .addField(':gear: General/Info', '`timer`, `ping`, `channelinfo`, `topinvite`, `botinfo`, `serverinfo`, `userinfo`, `serverinvite`, `guild`, `rolelist`, `level`, `avatar`')
